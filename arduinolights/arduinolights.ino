0e0bc83e6885e1b23fe7da9d14122d101bd65d35
178Laptop <programming@farmingtonrobotics.org>
2017-02-14 19:35:07 -0500

    Made Enforcers animation between blue and yellow lights yeah

--- arduinolights/arduinolights.ino
+++ arduinolights/arduinolights.ino
@@ -5,6 +5,8 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(36, 3, NEO_GRB + NEO_KHZ800);
 String myWord = "Blue Alliance";
 int subsystem_start[3]={0,12,24};
 int subsystem_end[3] = {11,23,35};
+int i = 0;
+
 
 void setup() {
   
@@ -33,11 +35,20 @@ void setSubsystemLights(int subsystem, String color){
     if (color == "green"){
      strip.setPixelColor(x, 0,255,0); 
     }
-    if (color == "red"){
+    else if (color == "red"){
       strip.setPixelColor(x, 255,0,0);
     }
-    if (color == "blue"){
+    else if (color == "blue"){
       strip.setPixelColor(x, 0, 0, 255);
+    }
+    else if (color == "Enforcers"){
+      if ((x%2 == 1 && i%2 == 1) || (x%2 == 0 && i%2 == 0)) {
+        strip.setPixelColor(x, 229, 187, 0);
+      }
+      else if ((x%2 == 1 && i%2 == 0) || (x%2 == 0 && i%2 == 1)){
+        strip.setPixelColor(x, 7, 16, 79);
+      }
+
     }
   }
 }
@@ -46,8 +57,8 @@ void setSubsystemLights(int subsystem, String color){
 void loop() {
   setSubsystemLights(2, "green");
   setSubsystemLights(1, "red");
-  setSubsystemLights(0, "red");
+  setSubsystemLights(0, "Enforcers");
   strip.show();
   delay(500);
-
+  i++;
 }
